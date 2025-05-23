05370c59b61ee65be01272952713837749b4d8bb
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-05-23 13:06:06 +0330

    making it happen

--- tunneling_tool/main.py
+++ tunneling_tool/main.py
@@ -3,4 +3,5 @@ import requests
 from uuid import uuid4
 from random import randint
 
-os.system("ssh -R 15000:localhost:11434 debian@tunnel.proximagemini.com")
+def generate_credentials():
+    pass
