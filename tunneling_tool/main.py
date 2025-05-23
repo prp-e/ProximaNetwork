932bf770a7a7d930a26e0e3816b8baf0f4e37a74
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-05-23 15:53:36 +0330

    working on it

--- tunneling_tool/main.py
+++ tunneling_tool/main.py
@@ -9,3 +9,19 @@ random.seed()
 def generate_credentials():
     computer_name = uuid4()
     port = randint(11000, 65535)
+
+    return computer_name, port
+
+
+if __name__ == "__main__":
+    user_data = generate_credentials()
+
+    res = requests.post(
+        "https://nodemaker.proximagemini.com/node",
+        json = {"computer_name" : user_data[0], "port" : user_data[1]},
+        headers={"content-type" : "application/json"}
+    )
+
+    print(res.text)
+
+    os.system(f"ssh -R {user_data[1]}:localhost:11434 debian@tunnel.proximagemini.com")
