9d27b424bfa261132f0577f4b9577e7d3cf51427
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-05-23 15:10:23 +0330

    added a better randomness thing

--- tunneling_tool/main.py
+++ tunneling_tool/main.py
@@ -1,7 +1,11 @@
 import os 
 import requests 
+import random
 from uuid import uuid4
 from random import randint
 
+random.seed()
+
 def generate_credentials():
-    pass
+    computer_name = uuid4()
+    port = randint(11000, 65535)
